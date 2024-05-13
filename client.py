5a338dd7d18fae9be0919f5b93fffa2b75ce474d
breinsprout <147308884+the-beansprout@users.noreply.github.com>
2024-05-13 21:35:54 +0800

    Add files via upload

---
+++ client.py
@@ -0,0 +1,40 @@
+import socket
+
+host = "localhost"
+port = 2244
+
+while True:
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    try:
+        s.connect((host, port))
+    except ConnectionRefusedError:
+        print("Connection refused. Make sure the server is running.")
+        exit()
+
+    try:
+        data = s.recv(1024)
+        print(data.decode().strip())
+        difficulty_choice = input("")
+        s.sendall(difficulty_choice.encode())
+
+        while True:
+            user_input = input("").strip()
+            s.sendall(user_input.encode())
+            reply = s.recv(1024).decode().strip()
+            if "Correct" in reply:
+                print(reply)
+                name = input("Enter your name: ")
+                s.sendall(name.encode())
+                s.sendall(difficulty_choice.encode())
+                break
+            print(reply)
+            continue
+    except Exception as e:
+        print("An error occurred:", e)
+    finally:
+        s.close()
+
+    play_again = input("Do you want to play again? (y/n): ")
+    if play_again.lower() != 'y':
+        break
